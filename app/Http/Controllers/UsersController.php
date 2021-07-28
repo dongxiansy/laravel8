bacf93ee74062bf44b87121720d79a571b22bc99
xdong <dongxiansy@163.com>
2021-07-28 14:51:14 +0800

    用户资料编辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,11 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
     /**
      * @throws ValidationException
      */
@@ -43,5 +48,30 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate(
+            $request,
+            [
+                'name'     => 'required|max:50',
+                'password' => 'nullable|confirmed|min:6'
+            ]
+        );
+
+        $data         = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
     }
 }
