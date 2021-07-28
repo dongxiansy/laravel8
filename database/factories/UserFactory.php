f70df63f24bbb237496ff93c692b9b0c4181d0d1
xdong <dongxiansy@163.com>
2021-07-28 17:42:38 +0800

    用户微博数据列表

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'activated' => true,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$1moqKChb0gZCYrYpnlghKO0iUuJYcV0CT6e7ZLyc5lmNl7CBLNQjO', // 123456
             'remember_token' => Str::random(10),
         ];
     }
